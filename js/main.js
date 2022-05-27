a363314a4ad4cb4c0cc235bed3d1faa6cd739b3f
m0la <57510729+m0lamibbleton@users.noreply.github.com>
2022-05-26 23:26:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
-  console.log('hello world')
+
   const userName = document.querySelector("#userName").value;
-  const userName2 = 'fight';
-  const res = await fetch(`/api?student=${userName}`);
-  const data = await res.json();
+  const res = await fetch(`/api?student=${userName}`)
+  const data = await res.json()
 
   console.log(data);
-  document.querySelector("#personStatus").textContent = pokemonText
-  // document.querySelector("#personName").textContent = data.name
-  // document.querySelector("#personStatus").textContent = data.status
+  document.querySelector("#personName").textContent = data.name
+  document.querySelector("#personStatus").textContent = data.type
   // document.querySelector("#personOccupation").textContent = data.currentOccupation
 }
